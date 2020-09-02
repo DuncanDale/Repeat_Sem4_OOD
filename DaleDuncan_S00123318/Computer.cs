cfd51af5a8689530d38dbcbbc5a2b96181f8f2f6
S00123318 <S00123318@VLAB-KSODVY2-34>
2020-09-02 10:29:52 +0100

    Q2-Properties

---
+++ DaleDuncan_S00123318/Computer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaleDuncan_S00123318
+{
+    public class Computer
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string Manufacturer_Image { get; set; }
+        public string Computer_Image { get; set; }
+        public string Memory { get; set; }
+        public string Processor { get; set; }
+    }
+}
